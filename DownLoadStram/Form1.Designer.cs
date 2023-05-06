42b230dab0e86fa568695492ac11bca9cf95e06b
ashraf <ashraf.m.ragab@gmail.com>
2023-05-06 19:57:12 +0300

    first commit

---
+++ DownLoadStram/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace DownLoadStram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DownLoad = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(422, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // DownLoad
+            // 
+            this.DownLoad.Location = new System.Drawing.Point(604, 33);
+            this.DownLoad.Name = "DownLoad";
+            this.DownLoad.Size = new System.Drawing.Size(153, 23);
+            this.DownLoad.TabIndex = 1;
+            this.DownLoad.Text = "DownLoad";
+            this.DownLoad.UseVisualStyleBackColor = true;
+            this.DownLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(161, 262);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(405, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(422, 24);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.DownLoad);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button DownLoad;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ DownLoadStram/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownLoadStram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+              progressBar1.Minimum = 0;
+            progressBar1.Maximum = 3000;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var urls = new[] {
+            textBox1.Text,
+           textBox2.Text
+            };
+            foreach (var item in urls)
+            {
+
+                var fileNme = new Random().Next(10);
+                var client = new HttpClient();
+
+                var request = new HttpRequestMessage(HttpMethod.Get, item);
+                //"https://nor-1.down-egy.xyz/The.Boss.Baby.Christmas.Bonus.2022.1080p.NF.WEB-DL.AR.mp4?Key=Jf-dNUC8uVWkE02rHuw0Yg&Expires=1683385598");use this link for test
+
+           
+                var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                var stream = await response.Content.ReadAsStreamAsync();
+                var buffer = new byte[2024];
+                var observable = Observable.FromAsync(() => stream.ReadAsync(buffer, 0, buffer.Length))
+                    .Repeat()
+                    .TakeWhile(count => count > 0)
+                    .Select(count => buffer.Take(count).ToArray())
+                    .ObserveOn(SynchronizationContext.Current)//use this for thread management to avoid context switch 
+                .Subscribe(
+                    buffers =>
+                    {
+                    // Process received data
+                    var fileStream = new FileStream("Movie_" + fileNme + ".mp4", FileMode.Append);
+                        using (var writer = new BinaryWriter(fileStream))
+                        {
+                            foreach (var buffero in buffers)
+                            {
+                                writer.Write(buffero);
+                            }
+                        }
+                        // Update progress bar
+                        UpdateProgresBar(buffers.Length);
+                    },
+                    ex => MessageBox.Show($"Error: {ex.Message}"),
+                    () => MessageBox.Show("Download completed.")
+                );
+            }
+            //;
+        }
+        void UpdateProgresBar(int value)
+        {
+            //label2.Text = value.ToString();
+            progressBar1.Value = value;
+        }
+    }
+}
